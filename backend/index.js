4105048648f33a960f2a1bb86f20f13fbd6a174a
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-07-11 12:16:08 -0400

    changes

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,11 @@ import Todo from './models/todo.js'
 const app = express()
 const port = process.env.PORT
 
+app.use(express.json()) //mw
 app.use(cors())
+ 
 
-app.get('/todo', async (req, res) => {
+app.get('/todos', async (req, res) => {
 try{
    const todos = await  Todo.find({})
     res.status(200).json(todos)
@@ -17,6 +19,18 @@ try{
     res.status(400).json(e)
 }
     
+})
+app.post('/todos', async (req, res) => {
+    try{
+  const todo =  await  Todo.create(req.body)
+  res.status(200).json(todo)
+//    const todo = new  Todo(req.body)
+//    await todo.save()
+    }catch(e){
+        console.log(e)
+        res.status(400).json(e)
+    }
+   
 })
 app.listen(port, () => {
     console.log('Listening on port: ' + port)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,64 @@
-// import { useState } from 'react'
-
-import { useEffect } from 'react'
+ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
 
-  useEffect(() => {
-    async function getData(){
-      try{
-      const response = await fetch('http://localhost:8080')
-    const data = await response.json()
-    console.log(data) //
-    }catch(e){
-      console.log(e)
-    }}
-    getData()
-  }, [])
-  
+  const [todos, setTodos] = useState([])
+  const inputRef = useRef()
 
+  useEffect(() => {
+
+    async function getData() {
+      try {
+        const response = await fetch('http://localhost:8080/todos')
+        const data = await response.json()
+        console.log(data) 
+        setTodos(data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    getData()
+
+  }, [])
+
+  console.log('todos: ', todos)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const todo = {
+      text: inputRef.current.value
+    }
+    
+    const response = await fetch('http://localhost:8080/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const newTodo = await response.json()
+
+    console.log(newTodo)
+
+    setTodos([...todos, newTodo])
+
+  }
+ 
   return (
     <>
-      Hello(from front end)
+      <h1>Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {todos.map((todo) => 
+          <li key={todo._id}>{todo.text}</li>
+        )}
+      </ul>
     </>
   )
 }
